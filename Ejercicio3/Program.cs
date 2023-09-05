cd6c15ca8c492fc65e84003cbd54b0322246972d
JulianSuarez5 <Juacosuarez12@gmail.com>
2023-09-04 22:27:17 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class TablaMultiplicar
+{
+    static void Main()
+    {
+        int numero, resultado;
+
+        Console.Write("Ingrese un número: ");
+        numero = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= 10; i++)
+        {
+            resultado = numero * i;
+            Console.WriteLine(numero + " x " + i + " = " + resultado);
+        }
+
+        Console.ReadKey();
+    }
+}
